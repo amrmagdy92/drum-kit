3d8f4d219180777444a8f9f4d3fb7d93eb52c29b
unknown <amr.magdy.ahmed92@gmail.com>
2024-02-16 17:46:33 +0200

    converted the if-else into switch

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 window.addEventListener('keypress', (e) => {
     let audio
-    if (e.key === "w") {
-        audio = new Audio('./sounds/tom-1.mp3')
-        audio.play()
-    } else if (e.key == "a") {
-        audio = new Audio('./sounds/tom-2.mp3')
-        audio.play()
-    } else if (e.key == "s") {
+    switch (e.key) {
+        case "w":
+            audio = new Audio('./sounds/tom-1.mp3')
+            audio.play()
+            break;
+
+        case "a":
+            audio = new Audio('./sounds/tom-2.mp3')
+            audio.play()
+            break;
+
+        case "s":
         audio = new Audio('./sounds/tom-3.mp3')
         audio.play()
-    } else if (e.key == "d") {
+        break;
+
+        case "d":
         audio = new Audio('./sounds/tom-4.mp3')
         audio.play()
-    } else if (e.key == "j") {
+        break;
+
+        case "j":
         audio = new Audio('./sounds/snare.mp3')
         audio.play()
-    } else if (e.key == "k") {
+        break;
+
+        case "k":
         audio = new Audio('./sounds/crash.mp3')
         audio.play()
-    } else if (e.key == "l") {
+        break;
+
+        case "l":
         audio = new Audio('./sounds/kick-bass.mp3')
         audio.play()
+        break;
+
+        default:
+            break;
     }
 })
 
